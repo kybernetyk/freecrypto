3225883a29f1c6d09dda3a4257c47aacc91d38cf
John Mac <charles.beck@sent.com>
2010-07-06 23:47:41 +0100

    Added little utility functions

--- util.go
+++ util.go
@@ -3,28 +3,37 @@
 
 package freecrypto
 
+import (
+	"encoding/binary"
+)
 
-// Turns from bits to bytes of different sizes.
 
-func BitsToBytes(n uint) uint {
+// Turns from bits.
+
+func BitToByte(n uint) uint {
 	return n >> 3 // n / 8
 }
 
-func BitsToWords16(n uint) uint {
+func BitToWord16(n uint) uint {
 	return n >> 4 // n / 16
 }
 
-func BitsToWords32(n uint) uint {
+func BitToWord32(n uint) uint {
 	return n >> 5 // n / 32
 }
 
-func BitsToWords64(n uint) uint {
+func BitToWord64(n uint) uint {
 	return n >> 6 // n / 64
 }
 
-// Turns from bytes of different sizes to bits.
+// Turns to bits.
 
-func BytesToBits(n uint) uint {
+func ByteToBit(n uint) uint {
 	return n << 3 // * 8
+}
+
+
+func IsPowerOf2(n uint) bool {
+	return n > 0 && (n & (n-1)) == 0
 }
 
